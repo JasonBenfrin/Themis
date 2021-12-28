a914d43a77fe83da8d8789c754617857650febdb
Bhone M M <86666641+JasonBenfrin@users.noreply.github.com>
2021-12-28 09:02:38 +0000

    Link to GitHub

--- commands/bot-info.js
+++ commands/bot-info.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('bot-info')
 		.setDescription('Sends a link to bot\'s code'),
 	async execute(interaction) {
-		return interaction.reply("Version: Release 1.0.1\n**Link to bot's code:\n** https://replit.com/@Bhone-MM/Encourage-Bot-js \n *Note: You can fork the repl to use it as your own code! Just make a secret variable for bot token.*");
+		return interaction.reply("Version: Release 1.0.1\n**Link to bot's code:\n** https://replit.com/@Bhone-MM/Encourage-Bot-js \nhttps://github.com/JasonBenfrin/Encourage-Bot\n *Note: You can fork the repl to use it as your own code! Just make a secret variable for bot token.*");
 	},
 };
