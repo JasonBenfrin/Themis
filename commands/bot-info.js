a37de3bb667800c52012eb2800c1c1a027914def
Bhone M M <86666641+JasonBenfrin@users.noreply.github.com>
2022-03-01 11:46:52 +0000

    Hyperlink Formats in bot-info.js

--- commands/bot-info.js
+++ commands/bot-info.js
@@ -5,6 +5,10 @@ module.exports = {
 		.setName('bot-info')
 		.setDescription('Sends a link to bot\'s code'),
 	async execute(interaction) {
-		return interaction.reply("Version: Release 1.0.1\n**Link to bot's code:\n** https://replit.com/@Bhone-MM/Encourage-Bot-js \nhttps://github.com/JasonBenfrin/Encourage-Bot\n *Note: You can fork the repl to use it as your own code! Just make a secret variable for bot token.*");
+		return interaction.reply(`Version: Release 1.0.1
+		\n**Link to bot's code:**
+		\n[**Github - JasonBenfrin - Encourage-Bot**](<https://github.com/JasonBenfrin/Encourage-Bot> "Github")
+		\n[**Replit - JasonBenfin - Encourage-Bot**](<https://replit.com/@Bhone-MM/Encourage-Bot-js> "Replit")
+		\n *Note: You can fork the repl to use it as your own code! Just make a secret variable for bot token.*`);
 	},
 };
