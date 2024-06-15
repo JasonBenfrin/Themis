02e6a01ca5fdac3b68a0758c638cbda60b60575c
Jason Benfrin <bhonemyatmin123@gmail.com>
2024-06-15 20:32:38 +0300

    Fix

--- commands/aternos/start.js
+++ commands/aternos/start.js
@@ -70,7 +70,6 @@ export default async function start(interaction) {
     await page.waitForSelector("#start")
     const ip = await page.evaluate('Object.values(document.querySelector(".server-ip").childNodes).filter(e => e.nodeType === 3).reduce((prev, curr) => prev + curr.textContent, "").trim()')
     await page.click("#start")
-    await page.waitForTimeout(1000)
     setTimeout(() => {
       message.edit({content: `Started ${ip}`, components:[]})
       collector.stop("end")
